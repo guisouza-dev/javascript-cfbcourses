47e96889dcd6592e47cf6955634c460059ec1e4a
Guilherme de Souza <112214045+guisouza-dev@users.noreply.github.com>
2022-09-29 15:08:44 -0300

    Aprendendo a mexer com switch case

--- aula14/aula14.js
+++ aula14/aula14.js
@@ -1,1 +1,26 @@
 "use strict"
+let ranking = 45
+let pódio = false
+
+switch (ranking) {
+    case 1: 
+        console.log(`1º Colocado!`)
+        console.log("PARABÉNS Você foi o campeão!!!💪🏆🥇")
+        pódio = true
+        break
+    case 2:
+        console.log(`2º Colocado!`)
+        console.log("Parabéns você foi o vice campeão!!🥈👏")
+        pódio = true
+        break
+    case 3:
+        console.log(`3º Colocado!`)
+        console.log("Parabéns você foi o terceiro colocado!!🥉👏")
+        pódio = true
+        break
+    default:
+        console.log(`Você ficou em ${ranking}º parabéns!`)
+}
+if (pódio === true){
+    console.log("Você subiu ao pódio!")
+}
