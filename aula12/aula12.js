7ae6c66a062593090be685e22c2f76741a7044c9
Guilherme de Souza <112214045+guisouza-dev@users.noreply.github.com>
2022-09-29 11:08:41 -0300

    Aprendendo a mexer com spreads

--- aula12/aula12.js
+++ aula12/aula12.js
@@ -25,9 +25,27 @@ let player3 = {
     ...player2
 }
 
+let valores = [15, 27, 89]
+
+let soma = (v1, v2, v3)=>{
+    return v1 + v2 + v3
+}
+
+let objs1 = document.getElementsByTagName('div')
+let objs2 = [...document.getElementsByTagName('div')]
+
 console.log("\n-=-=-=-=-=-=-=-=-=-= Spread -=-=-=-=-=-=-=-=-=-=\n")
 console.log("-º-º-º-º-º-º-º-º-º-º- ... -º-º-º-º-º-º-º-º-º-º-\n")
-console.log("O operador Spread(...) funciona muito bem para copiar outros elementos desde que sejam do mesmo tipo:")
+console.log("O operador Spread(...) funciona muito bem para copiar outros elementos:")
 console.log(`Player 1 ${player1.nickname}: Vidas ${player1.vidas}, Energia ${player1.energia}`)
-console.log(`Player 2 ${player2.nickname}: Vidas ${player2.vidas}, Energia ${player2.energia}\n`)
+console.log(`Player 2 ${player2.nickname}: Vidas ${player2.vidas}, Energia ${player2.energia}`)
 console.log(`Caso eu queira copiar as propriedades de ${player1.nickname} só preciso declarar com "..." antes: Player 1 ${player3.nickname}: Vidas ${player3.vidas}, Energia ${player3.energia}, Magia ${player3.magia}, Velocidade ${player3.velocidade}`)
+console.log(soma(...valores))
+
+console.log(`Esse código resultará em um HTML Collection que aparecerá abaixo:`)
+console.log(objs1)
+console.log(objs2)
+
+objs2.forEach(element => {
+    element.innerHTML = 'Mudei as divs via JavaScript'
+});
