d0e50ecbf3016ac4a5583265e55ef931560d6117
Guilherme de Souza <112214045+guisouza-dev@users.noreply.github.com>
2022-09-30 15:58:58 -0300

    Aprendendo arrow function

---
+++ aula25/aula25.js
@@ -0,0 +1,13 @@
+"use strict"
+
+const soma = (v1, v2) => {
+    let res = v1 + v2
+    return res
+}
+console.log(soma(15, 18))
+
+const nome = n => n
+console.log(nome('Guilherme de Souza'))
+
+const add = n => n + 40
+console.log(add(10))
