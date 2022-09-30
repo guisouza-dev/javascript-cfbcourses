08326ab8efe24cb75b514d13ce27682d69012ce8
Guilherme de Souza <112214045+guisouza-dev@users.noreply.github.com>
2022-09-30 14:15:21 -0300

    aprendendo os parâmetros REST

--- aula23/aula23.js
+++ aula23/aula23.js
@@ -1,1 +1,12 @@
 "use strict"
+let valorPadrão = 0
+
+function soma(...valores){
+    let res = 0
+    for(let elements of valores){
+        res += elements
+    }
+    return res
+}
+
+console.log(soma(15, 15, 30, 45, 78, 94, 68))
